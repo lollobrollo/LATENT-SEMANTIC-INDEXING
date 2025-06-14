4426309a7472e6466556cfffaa6f4ff7bb25915c
Lorenzo <lorenzo.tonet.03@gmail.com>
2025-06-14 12:15:27 +0200

    added the computation of document frequency

--- source/term_document_matrix.py
+++ source/term_document_matrix.py
@@ -2,23 +2,68 @@
     This code is developed as final project for the Information Retrieval course (2024 - 2025).
     Authors : Bortolussi L., Bredariol F., Tonet L.
 """
+
 import pandas as pd
 import numpy as np
 from collections import Counter
 
+def compute_doc_freq(td_matrix: np.array) -> np.array:
+    """
+    Counts the documents where a term appears
+
+    Parameters:
+    - td_matrix (np.array): a precomputed term document matrix. It can be "one-hot encoded" or "term frequencies".
+
+    Returns:
+    - np.array: "document frequency" for each term. The idx of terms matches the one of the matrix
+    """
+
+    return np.count_nonzero(td_matrix, axis=1)
+
+
 def create_vocab(docs: pd.DataFrame) -> list:
+    """
+    Creates a vocabulary list and computes term frequencies for each document.
+
+    Parameters:
+    - docs (pd.DataFrame): A pandas Series or list-like object containing documents as strings.
+
+    Returns:
+    - list: A sorted list of unique terms (vocabulary).
+    - list of dictionaries: A list where each element is a dictionary term:frequency for every document.
+    """
+
     vocab = []
     term_frequencies = []
+
+    # iterate every document in the list
     for doc in docs:
+        # get a list of single lemmas
         temp = doc.split(" ")
+        # get a dictionary {term:frequency}
         doc_tf = Counter(temp)
+
         term_frequencies.append(doc_tf)
+
+        #add all (unique) terms in the vocabulary
         vocab = vocab + list(set(temp))
 
     return list(set(vocab)), term_frequencies
 
-def build_term_documents_mat(df: pd.DataFrame, boolean_matrix : bool = False) -> np.array:
 
+def build_term_documents_mat(df: pd.DataFrame, boolean_matrix: bool = False) -> np.array:
+    """
+    Builds a term-document matrix from preprocessed text in a DataFrame.
+
+    Parameters:
+    - df (pd.DataFrame): DataFrame containing a 'clean_text' column with preprocessed text (space-separated tokens).
+    - boolean (bool): True for binary term presence instead of frequency.
+
+    Returns:
+    - np.array: A 2D NumPy array where rows are terms and columns are documents.
+                Each entry represents the frequency of a term in a document.
+    """
+        
     docs = df["clean_text"]
 
     vocab , term_freq = create_vocab(docs)
@@ -31,15 +76,16 @@ def build_term_documents_mat(df: pd.DataFrame, boolean_matrix : bool = False) ->
     for doc_idx, tf_doc in enumerate(term_freq):
         for term, freq in tf_doc.items():
 
-            if boolean_matrix:
-                freq = 1
+            if boolean_matrix: freq = 1
 
             term_document_matrix[indexed_vocab[term], doc_idx] = freq
 
-    return term_document_matrix.T, np.array(sorted_vocab)
+    return term_document_matrix, sorted_vocab
 
 # Example usage
 if __name__ == '__main__':
-    df = pd.read_parquet("./data/test/test.parquet")
+    df = pd.read_parquet(r"D:\UNI\3° ANNO\DATA VIS & INFORMATION RETRIEVAL\IR Project\LSI\data\test\test.parquet")
     td_matrix = build_term_documents_mat(df)
-    print(td_matrix)
+
+    print(td_matrix.shape)
+    
